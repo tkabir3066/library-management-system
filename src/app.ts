171373db94fee847888eda2cf3d06c10ed0ac714
Tutul Kabir <tutulkabir@gmail.com>
2025-07-10 20:19:53 +0530

    modified in server.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,24 @@
 import express, { Application, Request, Response } from "express";
 import { booksRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
-import cors from 'cors';
+import cors from "cors";
 // npm i --save-dev @types/cors
 
 const app: Application = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-    origin: ['http://localhost:5173',
-        "https://redux-library-management-system-by.vercel.app"
-    ]
-}))
+app.use(cors());
 
-app.use('/api/books', booksRoutes)
-app.use('/api/borrow', borrowRoutes)
+app.use("/api/books", booksRoutes);
+app.use("/api/borrow", borrowRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome from Library Management API')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome from Library Management API");
+});
 
 // 404 error
 app.use((req: Request, res: Response) => {
-    res.status(404).json({ message: 'Sorry! Route not found' })
-})
-
+  res.status(404).json({ message: "Sorry! Route not found" });
+});
 
 export default app;
